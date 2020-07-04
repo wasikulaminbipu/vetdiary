4028bd956a0cea81937181eaa4d79eb04f23168f
wasikulaminbipu <wasikulaminbipu@gmail.com>
2020-07-04 17:38:10 +0600

    Dosage Calculator Layout Created

--- lib/widgets/custom_input_row.dart
+++ lib/widgets/custom_input_row.dart
@@ -32,43 +32,47 @@ class _CustomInputRowState extends State<CustomInputRow> {
         children: <Widget>[
           Flexible(
             flex: 5,
-            child: Container(
+            child: FittedBox(
+              fit: BoxFit.cover,
                 // width: MediaQuery.of(context).size.width * 7 / 20,
                 child: Text(this.widget.title, style: Theme.of(context).primaryTextTheme.headline4,)),
           ),
           Flexible(
             flex: 3,
-            child: Container(
-                // width: MediaQuery.of(context).size.width * 3 / 20,
-                child: TextField(
-                  keyboardType: this.widget.keyboardType,
-                  onChanged: this.widget.onChanged,
-                  decoration: InputDecoration(contentPadding: EdgeInsets.symmetric(horizontal: 7.0, vertical: 0.0)),
-                  style: Theme.of(context).primaryTextTheme.headline4,
-                )),
+            fit: FlexFit.tight,
+            child: TextField(
+              keyboardType: this.widget.keyboardType,
+              onChanged: this.widget.onChanged,
+              decoration: InputDecoration(contentPadding: EdgeInsets.symmetric(horizontal: 7.0, vertical: 0.0)),
+              style: Theme.of(context).primaryTextTheme.headline4,
+            ),
           ),
+          if( this.widget.options != null ) SizedBox(width: 5.0,),
           if (this.widget.options != null && this.widget.options.length!= 0) Flexible(
                 flex: 3,
                 fit: FlexFit.loose,
                   // width: MediaQuery.of(context).size.width * 6 / 20,
-                  child: FittedBox(
-                    fit: BoxFit.fill,
-                    child: Container(
-                      padding: EdgeInsets.symmetric(horizontal: 10.0),
-                      decoration: BoxDecoration(
-                        border: Border.all(color: Colors.grey)
-                      ),
-                      height: 50.0,
-                      child: DropdownButton(
-                        value: this.dropdownItem,
-                        items: List<DropdownMenuItem>.from(this.widget.options.map((String e) => DropdownMenuItem(child: Text(e), value: e,))),
-                        onChanged: (data) {
-                          this.setState(() { this.dropdownItem = data.toString(); });
-                        },
+                  child: Container(
+                    child: FittedBox(
+                      fit: BoxFit.cover,
+                      child: Container(
+                        padding: EdgeInsets.symmetric(horizontal: 10.0),
+                        decoration: BoxDecoration(
+                          border: Border.all(color: Colors.grey)
+                        ),
+                        height: 50.0,
+                        child: DropdownButton(
+                          value: this.dropdownItem,
+                          items: List<DropdownMenuItem>.from(this.widget.options.map((String e) => DropdownMenuItem(child: Text(e), value: e,))),
+                          onChanged: (data) {
+                            this.setState(() { this.dropdownItem = data.toString(); });
+                          },
+                        ),
                       ),
                     ),
                   ),
                 ),
+                if (this.widget.lookup!=null) SizedBox(width: 5.0,) ,
           if (this.widget.lookup != null) Flexible(
             flex: 2,
             // width: MediaQuery.of(context).size.width * 3 / 20,
